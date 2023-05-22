45350610533673b736e569c1b657a3f301f6a337
pmfoysal <pmfoysal@gmail.com>
2023-05-22 12:46:32 +0600

    Social Share Buttons Added

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,21 +1,16 @@
 'use client';
+import Share from './share';
 import Link from 'next/link';
 import Preview from './preview';
-import { useEffect } from 'react';
-import { toast } from 'react-hot-toast';
 import getDate from '@/utilities/getDate';
 import { editBlog } from '@/utilities/api';
+import { useEffect, useState } from 'react';
 import { notFound, usePathname, useRouter } from 'next/navigation';
 
 export default function Details({ blog }) {
 	const router = useRouter();
 	const pathname = usePathname();
-
-	function handleShare() {
-		navigator.clipboard.writeText(location.href).then(() => {
-			toast.success('Link copied!');
-		});
-	}
+	const [isOpen, setIsOpen] = useState(false);
 
 	function handleEdit() {
 		if (pathname === '/') router.push('/editor');
@@ -76,14 +71,21 @@ export default function Details({ blog }) {
 								</svg> */}
 								Fork this Strain
 							</button>
-							<button type='button' title='Share' onClick={handleShare}>
-								<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
-									<path
-										fill='currentColor'
-										d='M17 2.498a3.502 3.502 0 1 1-2.597 5.851l-4.558 2.604a3.5 3.5 0 0 1 0 2.093l4.557 2.606a3.502 3.502 0 1 1-.745 1.302L9.1 14.347a3.502 3.502 0 1 1 0-4.698l4.557-2.604A3.502 3.502 0 0 1 17 2.498Zm0 13.5a2.002 2.002 0 1 0 0 4.004a2.002 2.002 0 0 0 0-4.004Zm-10.498-6a2.002 2.002 0 1 0 0 4.004a2.002 2.002 0 0 0 0-4.004Zm10.498-6a2.002 2.002 0 1 0 0 4.004a2.002 2.002 0 0 0 0-4.004Z'
-									/>
-								</svg>
-							</button>
+							<div className='relative'>
+								<button
+									type='button'
+									title='Share'
+									className='has-window'
+									onClick={() => setIsOpen(prev => !prev)}>
+									<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
+										<path
+											fill='currentColor'
+											d='M17 2.498a3.502 3.502 0 1 1-2.597 5.851l-4.558 2.604a3.5 3.5 0 0 1 0 2.093l4.557 2.606a3.502 3.502 0 1 1-.745 1.302L9.1 14.347a3.502 3.502 0 1 1 0-4.698l4.557-2.604A3.502 3.502 0 0 1 17 2.498Zm0 13.5a2.002 2.002 0 1 0 0 4.004a2.002 2.002 0 0 0 0-4.004Zm-10.498-6a2.002 2.002 0 1 0 0 4.004a2.002 2.002 0 0 0 0-4.004Zm10.498-6a2.002 2.002 0 1 0 0 4.004a2.002 2.002 0 0 0 0-4.004Z'
+										/>
+									</svg>
+								</button>
+								{isOpen ? <Share /> : null}
+							</div>
 						</aside>
 					</nav>
 					<Preview source={blog.content} />
