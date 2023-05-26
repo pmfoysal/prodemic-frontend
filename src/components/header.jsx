f3124bfd72e0f47f8f28a5ae54ae3b392dee99c2
pmfoysal <pmfoysal@gmail.com>
2023-05-26 17:20:01 +0600

    Mobile View Menu Feature Added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,17 @@
+'use client';
 import Theme from './theme';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+	const path = usePathname();
+	const [isOpen, setIsOpen] = useState('false');
+
+	useEffect(() => {
+		setIsOpen(false);
+	}, [path]);
+
 	return (
 		<header className='header'>
 			<main className='container'>
@@ -14,6 +24,16 @@ export default function Header() {
 					<Link href='/leaderboard'>Leaderboard</Link>
 					<Theme />
 				</nav>
+				<div className={`mobile-menus ${isOpen ? 'active' : ''}`}>
+					<button className={`hamburger ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(prev => !prev)}>
+						<span></span>
+					</button>
+					<nav className='header-menus'>
+						<Link href='/recents'>Recents</Link>
+						<Link href='/leaderboard'>Leaderboard</Link>
+						<Theme />
+					</nav>
+				</div>
 			</main>
 		</header>
 	);
